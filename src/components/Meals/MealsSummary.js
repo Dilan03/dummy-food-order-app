95e4d98175a3179c6a33a039b098277e035834da
Dilan Garcia <dilanngcia@gmail.com>
2022-10-27 06:38:29 -0600

    Meals component added

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './MealsSummary.module.css'
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+      <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam autem alias est atque commodi aut similique natus possimus iste asperiores veritatis expedita non, ratione reiciendis porro perspiciatis nam. Sint, repudiandae. </p>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam inventore, nisi praesentium ducimus ipsa itaque minima nihil est obcaecati optio, perferendis sed velit quo? Quidem consectetur suscipit distinctio debitis sit. </p>
+    </section>
+  )
+}
+
+export default MealsSummary
